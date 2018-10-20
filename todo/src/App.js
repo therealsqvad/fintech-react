e10615109f1b456b003ef085907cab0487a9357b
therealsqvad <nikita_klepov@mail.ru>
2018-10-21 00:10:35 +0700

    alpha version design

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,26 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './TCSlogo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <header className="App-header">
+      <div id="todolist">
+      <center>ToDo List:</center>
+      <ul>
+        <li>task1</li>
+        <li>task2</li>
+      </ul>
       </div>
+        <img src={logo} className="App-logo" alt="logo" />
+        <span id="count">Count: {}</span>
+        <span id="message"></span>
+        <br />
+        <button id="upButton">UP</button>
+        <button id="downButton">DOWN</button>
+        <button id="resetButton">RESET</button>
+      </header>
+    </div>
     );
   }
 }
