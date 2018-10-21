7a2263bcda8df2cd4ccbdfc107065cf970b5ef09
therealsqvad <nikita_klepov@mail.ru>
2018-10-21 16:38:01 +0700

    all done

--- todo/src/App.js
+++ todo/src/App.js
@@ -4,37 +4,59 @@ import './App.css';
 
 class App extends Component {
 
-  state = { tasks: [], inputed: ''};
+  state = { tasks: [], inputed: '' };
 
-addTask = (e) => {
-  e.preventDefault()
-   this.setState({tasks: [...this.state.tasks, this.state.inputed], inputed: ''});
-}
+  addTask = (e) => {
+    e.preventDefault()
+    this.setState({ tasks: [...this.state.tasks, this.state.inputed], inputed: '' });
+  }
 
-inputchange = (e) => {
-  this.setState({inputed: e.target.value});
-}
+  inputchange = (e) => {
+    this.setState({ inputed: e.target.value });
+  }
+
+  delTask(deletedTask) {
+    this.setState(prevState => ({
+      tasks: prevState.tasks.filter(task => task !== deletedTask)
+    }));
+  }
+
+  changetask(task) {
+    let arr = this.state.tasks;
+    let index = arr.indexOf(task);
+    let temp = prompt('Введите новое задание:');
+    if (temp !== '') {
+      arr[index] = temp;
+    }
+    this.setState({
+      tasks: arr
+    });
+  }
 
   render() {
     return (
       <div className="App">
-      <header className="App-header">
-      
-        <img src={logo} className="App-logo" alt="logo" />
-        <form onSubmit={this.addTask}>
-        <input type="textarea" id="inputtext" maxLength="60" value={this.state.inputed} onChange={this.inputchange}></input>
-        <button id="add">Добавить</button>
-        </form>
-        <p>ToDo List:</p>
-        <div id="todolist">
+        <header className="App-header">
 
-      <ul id="taskList">
-        <li>{this.state.tasks}</li>
-        <li>task2</li>
-      </ul>
+          <img src={logo} className="App-logo" alt="logo" />
+          <form onSubmit={this.addTask}>
+            <input type="textarea" id="inputtext" maxLength="60" value={this.state.inputed} onChange={this.inputchange}></input>
+            <button id="add">Добавить</button>
+          </form>
+          <p>ToDo List:</p>
+          <div id="todolist" hidden={this.state.tasks.length === 0}>
+
+            <ul id="taskList">
+              {
+                this.state.tasks.map((task, index) => <li key={index} id={index} >
+                <span onClick={this.changetask.bind(this, task)}>{task}</span>
+                  <button className='delbtn' onClick={this.delTask.bind(this, task)}><b>X</b></button>
+                </li>)
+              }
+            </ul>
+          </div>
+        </header>
       </div>
-      </header>
-    </div>
     );
   }
 }
