0aaf034193b865c094880e57a26c663d01695885
therealsqvad <nikita_klepov@mail.ru>
2018-10-21 14:01:06 +0700

    add task inline

--- todo/src/App.js
+++ todo/src/App.js
@@ -3,24 +3,36 @@ import logo from './TCSlogo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = { tasks: [], inputed: ''};
+
+addTask = (e) => {
+  e.preventDefault()
+   this.setState({tasks: [...this.state.tasks, this.state.inputed], inputed: ''});
+}
+
+inputchange = (e) => {
+  this.setState({inputed: e.target.value});
+}
+
   render() {
     return (
       <div className="App">
       <header className="App-header">
-      <div id="todolist">
-      <center>ToDo List:</center>
-      <ul>
-        <li>task1</li>
+      
+        <img src={logo} className="App-logo" alt="logo" />
+        <form onSubmit={this.addTask}>
+        <input type="textarea" id="inputtext" maxLength="60" value={this.state.inputed} onChange={this.inputchange}></input>
+        <button id="add">Добавить</button>
+        </form>
+        <p>ToDo List:</p>
+        <div id="todolist">
+
+      <ul id="taskList">
+        <li>{this.state.tasks}</li>
         <li>task2</li>
       </ul>
       </div>
-        <img src={logo} className="App-logo" alt="logo" />
-        <span id="count">Count: {}</span>
-        <span id="message"></span>
-        <br />
-        <button id="upButton">UP</button>
-        <button id="downButton">DOWN</button>
-        <button id="resetButton">RESET</button>
       </header>
     </div>
     );
